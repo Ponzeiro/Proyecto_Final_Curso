48ed3e9538f896aca03076140c5a27511a0c4467
Ponzeiro <AbrahamBlancoRD@gmail.com>
2026-05-25 18:14:12 +0200

    test(backend): add application context test

---
+++ table-tale-backend/src/test/java/com/tabletale/backend/TableTaleBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tabletale.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTaleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
