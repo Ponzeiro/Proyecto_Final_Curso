d658ed47236380162cd314a94c2603c251d989bb
Ponzeiro <AbrahamBlancoRD@gmail.com>
2026-05-25 18:12:51 +0200

    feat(backend): initialize Spring Boot project

---
+++ table-tale-backend/src/main/java/com/tabletale/backend/TableTaleBackendApplication.java
@@ -0,0 +1,12 @@
+package com.tabletale.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableTaleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableTaleBackendApplication.class, args);
+    }
+}
